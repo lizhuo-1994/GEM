d466b47cf89239863fcb74fc8e04639667c16a7f
lizhuo-1994 <guitar_318576604@yahoo.co.jp>
2022-05-09 02:17:35 +0900

    update

--- gem_mujoco/run/run_swimmer_rcs_1.sh
+++ gem_mujoco/run/run_swimmer_rcs_1.sh
@@ -13,8 +13,8 @@ declare -a steps=(400001 400001 400001 100001 100001)
 declare -a envs_alias=("hopper" "walker" "swimmer" "doublependulum" "pendulum")
 export PYTHONPATH=$root
 
-OPENAI_LOGDIR=./log_gem/mujoco/rcs_1 python3 $root/run/train.py --num-timesteps=400001 --max_steps=1000 --agent=RCS --comment=swimmer_rcs_0 --seed 0 --env-id=Swimmer-v3 --order 1 --grid_num 10 --decay 0.1 --state_min -10 --state_max 10 --mode state_action 
-OPENAI_LOGDIR=./log_gem/mujoco/rcs_1 python3 $root/run/train.py --num-timesteps=400001 --max_steps=1000 --agent=RCS --comment=swimmer_rcs_1 --seed 1 --env-id=Swimmer-v3 --order 1 --grid_num 10 --decay 0.1 --state_min -10 --state_max 10 --mode state_action 
-OPENAI_LOGDIR=./log_gem/mujoco/rcs_1 python3 $root/run/train.py --num-timesteps=400001 --max_steps=1000 --agent=RCS --comment=swimmer_rcs_2 --seed 2 --env-id=Swimmer-v3 --order 1 --grid_num 10 --decay 0.1 --state_min -10 --state_max 10 --mode state_action 
-OPENAI_LOGDIR=./log_gem/mujoco/rcs_1 python3 $root/run/train.py --num-timesteps=400001 --max_steps=1000 --agent=RCS --comment=swimmer_rcs_3 --seed 3 --env-id=Swimmer-v3 --order 1 --grid_num 10 --decay 0.1 --state_min -10 --state_max 10 --mode state_action 
-OPENAI_LOGDIR=./log_gem/mujoco/rcs_1 python3 $root/run/train.py --num-timesteps=400001 --max_steps=1000 --agent=RCS --comment=swimmer_rcs_4 --seed 4 --env-id=Swimmer-v3 --order 1 --grid_num 10 --decay 0.1 --state_min -10 --state_max 10 --mode state_action 
+OPENAI_LOGDIR=./log_gem/mujoco/rcs_1 python3 $root/run/train.py --num-timesteps=400001 --max_steps=1000 --agent=RCS --comment=swimmer_rcs_0 --seed 0 --env-id=Swimmer-v3 --order 1 --grid_num 10 --decay 0.2 --state_min -10 --state_max 10 --mode state_action 
+OPENAI_LOGDIR=./log_gem/mujoco/rcs_1 python3 $root/run/train.py --num-timesteps=400001 --max_steps=1000 --agent=RCS --comment=swimmer_rcs_1 --seed 1 --env-id=Swimmer-v3 --order 1 --grid_num 10 --decay 0.2 --state_min -10 --state_max 10 --mode state_action 
+OPENAI_LOGDIR=./log_gem/mujoco/rcs_1 python3 $root/run/train.py --num-timesteps=400001 --max_steps=1000 --agent=RCS --comment=swimmer_rcs_2 --seed 2 --env-id=Swimmer-v3 --order 1 --grid_num 10 --decay 0.2 --state_min -10 --state_max 10 --mode state_action 
+OPENAI_LOGDIR=./log_gem/mujoco/rcs_1 python3 $root/run/train.py --num-timesteps=400001 --max_steps=1000 --agent=RCS --comment=swimmer_rcs_3 --seed 3 --env-id=Swimmer-v3 --order 1 --grid_num 10 --decay 0.2 --state_min -10 --state_max 10 --mode state_action 
+OPENAI_LOGDIR=./log_gem/mujoco/rcs_1 python3 $root/run/train.py --num-timesteps=400001 --max_steps=1000 --agent=RCS --comment=swimmer_rcs_4 --seed 4 --env-id=Swimmer-v3 --order 1 --grid_num 10 --decay 0.2 --state_min -10 --state_max 10 --mode state_action 
